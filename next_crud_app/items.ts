8dd28321d2f4cbbb6d4464ecb90e781d25bbca10
suryadev78 <pandeysuryadev00@gmail.com>
2025-09-21 23:04:29 +0530

    added animated navbar

---
+++ next_crud_app/items.ts
@@ -0,0 +1,25 @@
+
+"use client"
+import {Home,FileText,CreditCard} from "lucide-react"
+export const Items =[
+    {
+        name:"Home",
+        url:"/",
+        icon: Home
+        
+    },
+    {
+        name:"About"
+        ,
+        url:"/about",
+        icon: FileText
+    },
+
+
+
+    {
+        name:"Contact",
+        url:"/contact",
+        icon: CreditCard
+    }
+]
